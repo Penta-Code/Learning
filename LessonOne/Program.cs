75759dd485db2cb211c9e9b375912b822ac0dd93
Yuri Svechnikov <YSvechnikov@actsoft.com>
2021-10-23 06:08:55 -0700

    Task 1

--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -1,34 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Address MyAddress = new Address("72056", "USA", "Tampa", "Bane Ave", "14", "82");
+Address MyAddress = new Address("72056", "US", "Tampa", "Bane Ave", "14", "82");
 
 Address MyAddress2 = new Address();
+MyAddress2.Index = "74558";
+MyAddress2.Country = "US";
+MyAddress2.City = "Dallas";
+MyAddress2.Street = "Chopen St.";
+MyAddress2.House = "28";
+MyAddress2.Appartment = "122";
 
-Address MyAddress3 = new Address { Index = "55894", City = "NY" };
+Address MyAddress3 = new Address { Index = "55894", Country = "US", City = "NY", Street = "Sunny Ave", House = "46", Appartment = "72" };
 
-new Address().Show();
+Console.WriteLine($"Index: {MyAddress.Index}");
+Console.WriteLine($"Country: {MyAddress.Country}");
+Console.WriteLine($"City: {MyAddress.City}");
+Console.WriteLine($"Street: {MyAddress.Street}");
+Console.WriteLine($"House: {MyAddress.House}");
+Console.WriteLine($"Appartment: {MyAddress.Appartment}");
 
-Print($"Index: {MyAddress.Index}");
-Print($"Country: {MyAddress.Country}");
-Print($"City: {MyAddress.City}");
-Print($"Street: {MyAddress.Street}");
-Print($"House: {MyAddress.House}");
-Print($"Appartment: {MyAddress.Appartment}");
+Console.WriteLine($"\nIndex: {MyAddress2.Index}");
+Console.WriteLine($"Country: {MyAddress2.Country}");
+Console.WriteLine($"City: {MyAddress2.City}");
+Console.WriteLine($"Street: {MyAddress2.Street}");
+Console.WriteLine($"House: {MyAddress2.House}");
+Console.WriteLine($"Appartment: {MyAddress2.Appartment}");
 
-PrintO(MyAddress2);
+Console.WriteLine($"\nIndex: {MyAddress3.Index}");
+Console.WriteLine($"Country: {MyAddress3.Country}");
+Console.WriteLine($"City: {MyAddress3.City}");
+Console.WriteLine($"Street: {MyAddress3.Street}");
+Console.WriteLine($"House: {MyAddress3.House}");
+Console.WriteLine($"Appartment: {MyAddress3.Appartment}");
 
-PrintO(MyAddress3);
-
-
-void Print(string text)
-{
-    Console.WriteLine(text);
-}
-
-void PrintO(object item)
-{
-    Console.WriteLine(item);
-}
 
 class Address
 {
@@ -58,6 +62,6 @@ class Address
 
     public override string ToString()
     {
-        return "Default constructor";
+        return "Overriden constr";
     }
 }
