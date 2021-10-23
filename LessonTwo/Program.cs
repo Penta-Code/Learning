9472441baf87531d6d4410954afb9dbd3ce4fe45
Yuri Svechnikov <YSvechnikov@actsoft.com>
2021-10-23 07:11:02 -0700

    Task2

--- LessonTwo/Program.cs
+++ LessonTwo/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Rectangle
+{
+    double side1, side2;
+
+    double Side1 { get; }
+    double Side2 { get; }
+
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    double AreaCalculator(double side1, double side2)
+    {
+        double S = 0.5 * side1 * side2;
+        return S;
+    }
+
+    double PerimetrCalculator(double side1, double side2)
+    {
+        double perimetr = (side1 * 2) + (side2 * 2);
+        return perimetr;
+    }
+}
