52255b63a50300092ce6df6f713e9a7b3126333e
Yuri Svechnikov <YSvechnikov@actsoft.com>
2021-12-11 06:50:50 -0800

    learning

---
+++ LessonSix/Program.cs
@@ -0,0 +1,9 @@
+﻿using LessonSix;
+
+User yuri = new User("Yuri", "S", 32, "12/8/2021");
+
+Console.WriteLine(yuri);
+
+yuri.age = 35;
+
+yuri.Info();
---
+++ LessonSix/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonSix
+{
+    internal class User
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public readonly string fillingDate;
+
+        public User(string name, string surname, int age, string date)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.fillingDate = date;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"User info:\n{name}\n{surname}\n{age}\n{fillingDate}");
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"User info:\n{name}\n{surname}\n{age}\n{fillingDate}";
+        //}
+    }
+}
